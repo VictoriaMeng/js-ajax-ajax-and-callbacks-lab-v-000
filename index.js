9c2fc11224a891422d15463095d490e3c4181520
Victoria Meng <nights1ststar@protonmail.com>
2018-08-31 16:17:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ $(document).ready(function (){
 
 function searchRepositories() {
   const req = new XMLHttpRequest();
+  const searchTerms = document.getElementById("searchTerms").value
   debugger;
 }
