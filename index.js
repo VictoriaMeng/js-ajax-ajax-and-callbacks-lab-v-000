ae404dfeebc0721a78106553fc4eb3cb11b0e53f
Victoria Meng <nights1ststar@protonmail.com>
2018-08-31 16:21:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ $(document).ready(function (){
 
 function searchRepositories() {
   const req = new XMLHttpRequest();
-  const searchTerms = document.getElementById("searchTerms").value(" ");
-  debugger;
+  const searchTerms = document.getElementById("searchTerms");
+  req.addEventListener("load", showRepositories);
+  req.open("GET", `https://api.github.com/search/repositories/?q=${searchTerms}/`);
+  req.send();
 }
